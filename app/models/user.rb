df539af84087f68b4fb3ad75784bf58b4484602c
Zach Ewert <zachanima@gmail.com>
2010-02-28 13:57:16 +0100

    Validate uniqueness of username per shop

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,6 +3,7 @@ class User < ActiveRecord::Base
 
   belongs_to :shop
   validates_presence_of :first_name, :username, :password
+  validates_uniqueness_of :username, :scope => :shop_id
 
   def name
     [self.first_name, self.last_name].join(' ')
