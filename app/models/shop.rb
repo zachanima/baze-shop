533253e5bd78de7886b863a89f61a9023940d7d6
Zach Ewert <zachanima@gmail.com>
2010-03-03 11:45:26 +0100

    Validate uniqueness of shop link

--- app/models/shop.rb
+++ app/models/shop.rb
@@ -2,6 +2,7 @@ class Shop < ActiveRecord::Base
   has_many :categories, :dependent => :destroy
   has_many :users, :dependent => :destroy
   validates_presence_of :link
+  validates_uniqueness_of :link
 
   def orders
     self.users.collect { |user| user.orders }.flatten
