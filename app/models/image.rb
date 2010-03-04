1c320d03123a18aacf07b49678dbaf5ad3551780
Zach Ewert <zachanima@gmail.com>
2010-03-04 06:28:05 +0100

    Prettify product images a bit

--- app/models/image.rb
+++ app/models/image.rb
@@ -3,7 +3,7 @@ class Image < ActiveRecord::Base
   validates_presence_of :name
 
   has_attached_file :data, :styles => {
-    :small => '200x200>',
+    :small => '200x200#',
     :original => '800x600>' }
   validates_attachment_presence :data
   validates_attachment_content_type :data, :content_type => [
