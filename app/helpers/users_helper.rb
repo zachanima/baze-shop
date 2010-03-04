b1d24ce77a1cad6cce1a19865d2e08c08ca9c27e
Zach Ewert <zachanima@gmail.com>
2010-03-04 08:02:25 +0100

    Avoid capitalize in users helper attrs for import

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -1,7 +1,7 @@
 module UsersHelper
   def attributes_for_import
     options = [:first_name, :last_name, :username, :password, :department].collect do |a|
-      [a.capitalize, a]
+      [a, a]
     end
     options_for_select(options.unshift(['', '']))
   end
