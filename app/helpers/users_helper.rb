e927d1b7aa5cdb7bbd03d9440dbc93192e5db706
Zach Ewert <zachanima@gmail.com>
2010-03-08 18:21:59 +0100

    Add text to user attributes for import

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -1,6 +1,6 @@
 module UsersHelper
   def attributes_for_import
-    options = [:first_name, :last_name, :login, :password, :department].collect do |a|
+    options = [:first_name, :last_name, :login, :password, :department, :text].collect do |a|
       [a, a]
     end
     options_for_select(options.unshift(['', '']))
