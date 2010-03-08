261c085b8ee8563bf67c0daaca1103bb53fadffc
Zach Ewert <zachanima@gmail.com>
2010-03-08 23:58:25 +0100

    Clean up views related to product

--- app/models/product.rb
+++ app/models/product.rb
@@ -5,4 +5,26 @@ class Product < ActiveRecord::Base
   has_and_belongs_to_many :images
   has_and_belongs_to_many :option_groups
   has_and_belongs_to_many :options
+
+  def name
+    self.template.name
+  end
+
+  def number
+    self.template.number
+  end
+
+  def brand
+    self.template.brand
+  end
+
+  def text
+    self.template.text
+  end
+
+  def thumbnail
+    if self.images
+      self.images.first.data.url(:small)
+    end
+  end
 end
