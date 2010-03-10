5ef36d39880f5175b2fc79ccfeec7ce6fd2c1ba9
Zach Ewert <zachanima@gmail.com>
2010-03-10 14:23:22 +0100

    Fix shop orders index view

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -11,7 +11,7 @@ class OrdersController < ApplicationController
       render_shop
     else
       authenticate
-      @orders = @shop ? @shop.orders.all(:conditions => { :accepted => true }) : Order.all(:conditions => { :accepted => true })
+      @orders = @shop ? @shop.orders.select { |o| o.accepted = true } : Order.all(:conditions => { :accepted => true })
     end
   end
 
