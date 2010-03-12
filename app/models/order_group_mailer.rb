a5e05a591e46e6cc634e9365818aac02559c4b66
Zach Ewert <zachanima@gmail.com>
2010-03-12 10:47:11 +0100

    Add user email address to mailer receipt bcc

--- app/models/order_group_mailer.rb
+++ app/models/order_group_mailer.rb
@@ -1,7 +1,7 @@
 class OrderGroupMailer < ActionMailer::Base
   def receipt(order_group)
     recipients  'os@baze.dk'
-    bcc         order_group.user.shop.email_addresses.empty? ? nil : order_group.user.shop.email_addresses.collect { |e| "#{e.name} <#{e.text}>" }
+    bcc         [order_group.user.shop.email_addresses.empty? ? nil : order_group.user.shop.email_addresses.collect { |e| "#{e.name} <#{e.text}>" }, order_group.user.email].flatten.compact
     from        'Baze A/S <info@baze.dk>'
     subject     "#{order_group.user.shop.name}, Ordre ##{order_group.id}"
     sent_on     Time.now
