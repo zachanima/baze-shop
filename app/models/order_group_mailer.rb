3db4ff5d1a0f6cb2f6b50f344ccd87956176fdc6
Zach Ewert <zachanima@gmail.com>
2010-03-23 01:05:14 +0100

    Prettify mail receipt

--- app/models/order_group_mailer.rb
+++ app/models/order_group_mailer.rb
@@ -6,5 +6,6 @@ class OrderGroupMailer < ActionMailer::Base
     subject     "#{order_group.user.shop.name}, Ordre ##{order_group.id}"
     sent_on     Time.now
     body        :order_group => order_group, :shop => order_group.user.shop, :user => order_group.user
+    content_type 'text/html'
   end
 end
