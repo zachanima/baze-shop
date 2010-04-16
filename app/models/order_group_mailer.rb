878d77313d7feb00eb092ab4ddca039aa5399533
Zach Ewert <zachanima@gmail.com>
2010-04-16 03:58:57 +0200

    Remove receipt net price parentheses on no gross

--- app/models/order_group_mailer.rb
+++ app/models/order_group_mailer.rb
@@ -1,6 +1,6 @@
 class OrderGroupMailer < ActionMailer::Base
   def receipt(order_group)
-    recipients  'zachanima@gmail.com'
+    recipients  'os@baze.dk'
     bcc         order_group.user.email_address.nil? ? nil : "#{order_group.user.email_address.name} <#{order_group.user.email_address.text}>"
     from        'Baze A/S <info@baze.dk>'
     subject     "#{order_group.user.shop.name}, Ordre ##{order_group.id}"
