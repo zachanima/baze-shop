872711d9760d6303814645db5a28d8efbf4564a0
Zach Ewert <zachanima@gmail.com>
2010-05-17 20:32:32 +0200

    Reset master receipt recipient

--- app/models/order_group_mailer.rb
+++ app/models/order_group_mailer.rb
@@ -1,8 +1,6 @@
 class OrderGroupMailer < ActionMailer::Base
   def receipt(order_group)
-    #recipients  'os@baze.dk'
-    recipients  'zachanima@gmail.com'
-    #bcc         order_group.user.email_address.nil? ? nil : "#{order_group.user.email_address.name} <#{order_group.user.email_address.text}>"
+    recipients  'os@baze.dk'
     bcc         (order_group.user.email_address.nil? or order_group.user.email_address.net.blank?) ? nil : "#{order_group.user.email_address.name} <#{order_group.user.email_address.text}>"
     from        'Baze A/S <info@baze.dk>'
     subject     "#{order_group.user.shop.name}, Ordre ##{order_group.id}"
