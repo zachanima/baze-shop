c942f9457f03f58c62bc657edcce44de089edf10
Zach Ewert <zachanima@gmail.com>
2010-05-19 14:40:03 +0200

    Show price decimals on receipts, in cart

--- app/models/variation.rb
+++ app/models/variation.rb
@@ -4,6 +4,12 @@ class Variation < ActiveRecord::Base
   has_and_belongs_to_many :orders
   
   def text_with_price(price = nil, net_price = nil)
+    if price.is_a?(Float)
+      price = price ? ('%.2f' % price).gsub!('.', ',') : nil
+    end
+    if net_price.is_a?(Float)
+      net_price = net_price ? ('%.2f' % price).gsub!('.', ',') : nil
+    end
     [self.option.text, price ? " +#{price}" : nil, net_price ? " (#{net_price})" : nil].compact.join(' ')
   end
 end
