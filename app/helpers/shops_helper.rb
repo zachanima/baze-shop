86170a80c7f9c6f0af948307103174c72b0c7907
Zach Ewert <zachanima@gmail.com>
2010-06-07 01:42:47 +0200

    Add truncated status method to shops helper

--- app/helpers/shops_helper.rb
+++ app/helpers/shops_helper.rb
@@ -1,2 +1,15 @@
 module ShopsHelper
+  def truncated_status(shop)
+    status = shop.status
+    if status.count > 1
+      [
+        status.shift,
+        ', <span class="tooltip" title="',
+        status.join(', '),
+        '">&hellip;</span>'
+      ].join
+    else
+      status.first
+    end
+  end
 end
