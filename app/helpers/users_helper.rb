7d35fb7732e7f8d8192a3ce799cf5cbc49670dba
Zach Ewert <zachanima@gmail.com>
2010-06-07 05:43:24 +0200

    Add various link to methods to users helper

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -9,4 +9,20 @@ module UsersHelper
   def list_users_text
     @shop ? ['List users of', @shop.name].join(' ') : 'List all users'
   end
+
+  def link_to_add_user
+    link_to('Add user', @shop ? new_shop_user_path(@shop) : new_user_path)
+  end
+
+  def link_to_import_users
+    link_to('Import users', @shop ? import_shop_users_path(@shop) : import_users_path)
+  end
+
+  def link_to_user_name(user)
+    link_to(user.name, edit_shop_user_path(user.shop, user))
+  end
+
+  def link_to_user_orders(user)
+    link_to(user.order_groups.count.zero_text, shop_user_orders_path(user.shop, user))
+  end
 end
