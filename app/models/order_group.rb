e5cd1ca34af17104460831a82697a5f0574e09e7
Zach Ewert <zachanima@gmail.com>
2010-06-09 02:28:57 +0200

    Add price method to order group model

--- app/models/order_group.rb
+++ app/models/order_group.rb
@@ -2,4 +2,8 @@ class OrderGroup < ActiveRecord::Base
   has_many :orders
   belongs_to :user
   belongs_to :address
+
+  def price
+    self.orders.collect { |o| o.price }.compact.inject(0) { |b,i| b + i } unless self.dummy
+  end
 end
