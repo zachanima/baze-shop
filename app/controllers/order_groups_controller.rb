1c7371a35a27d01c40da74eb2982994473e644c7
Zach Ewert <zachanima@gmail.com>
2010-06-09 03:07:03 +0200

    Add multiple destroy to order groups controller

--- app/controllers/order_groups_controller.rb
+++ app/controllers/order_groups_controller.rb
@@ -14,4 +14,21 @@ class OrderGroupsController < ApplicationController
     OrderGroup.find(params[:id]).destroy
     redirect_to(@shop ? shop_order_groups_path(@shop) : order_groups_path)
   end
+
+  def multiple
+    if params[:order_group_ids].nil?
+      flash[:error] = 'No orders selected'
+    elsif params[:destroy]
+      multiple_destroy
+      flash[:notice] = "Deleted #{params[:order_group_ids].count} orders"
+    else
+      flash[:error] = "Unexpected error, params: #{params.inspect}"
+    end
+    redirect_to(@shop ? shop_order_groups_path(@shop) : order_groups_path)
+  end
+
+  private
+  def multiple_destroy
+    OrderGroup.destroy(params[:order_group_ids])
+  end
 end
