038db5b2e552d3acdc301fec11897733d75a43e7
Zach Ewert <zachanima@gmail.com>
2010-07-17 20:33:48 +0200

    Remove id column from user groups users join

--- db/migrate/20100717144308_add_user_groups_users_join.rb
+++ db/migrate/20100717144308_add_user_groups_users_join.rb
@@ -1,6 +1,6 @@
 class AddUserGroupsUsersJoin < ActiveRecord::Migration
   def self.up
-    create_table :user_groups_users do |t|
+    create_table :user_groups_users, :id => false do |t|
       t.references :user_group
       t.references :user
     end
