791e4c26f3b44c21350b57d6647509181749e024
Zach Ewert <zachanima@gmail.com>
2010-07-23 13:20:20 +0200

    Separate categories in tooltip by commas

--- app/helpers/products_helper.rb
+++ app/helpers/products_helper.rb
@@ -3,14 +3,14 @@ module ProductsHelper
     if product.categories.count > 0
       categories = product.categories
       text = categories.shift.self_and_ancestors.collect(&:name) * ' > '
-      tooltip = String.new
+      tooltip = Array.new
       categories.each do |category|
-        tooltip << category.self_and_ancestors.collect(&:name) * ' > '
+        tooltip << [category.self_and_ancestors.collect(&:name) * ' > ']
       end
       if tooltip.empty?
         text
       else
-        truncated_tooltip(tooltip, text)
+        truncated_tooltip(tooltip.join(', '), text)
       end
     end
   end
