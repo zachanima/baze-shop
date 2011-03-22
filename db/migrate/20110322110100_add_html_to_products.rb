9da51b830add106be1235343c9a7d5a748031c6c
Zach Ewert <zachanima@gmail.com>
2011-03-22 11:11:46 +0100

    Fix column name in add html migration

--- db/migrate/20110322110100_add_html_to_products.rb
+++ db/migrate/20110322110100_add_html_to_products.rb
@@ -1,9 +1,9 @@
 class AddHtmlToProducts < ActiveRecord::Migration
   def self.up
-    add_column :products, :markup, :text
+    add_column :products, :html, :text
   end
 
   def self.down
-    remove_column :products, :markup
+    remove_column :products, :html
   end
 end
